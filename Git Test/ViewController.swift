fdf7452b2a05fb5f316534b6a1dae3e0eaab98f3
Atil Samancioglu <samancioglu.atil@gmail.com>
2017-06-30 17:39:04 +0300

    Initial Commit

---
+++ Git Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Git Test
+//
+//  Created by Atil Samancioglu on 30/06/2017.
+//  Copyright © 2017 Atil Samancioglu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
