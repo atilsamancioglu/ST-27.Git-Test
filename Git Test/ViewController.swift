07a20cfe5e724f2c4ccb5f98dfa3e4a24e1d41e7
Atil Samancioglu <samancioglu.atil@gmail.com>
2017-06-30 17:43:29 +0300

    my first commit

--- Git Test/ViewController.swift
+++ Git Test/ViewController.swift
@@ -12,14 +12,11 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        print("github")
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+    
 
 }
 
